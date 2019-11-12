3ebe0d589964b6730779f52c0364daa475cc9bd2
Bruno Morais <brunosmmm@gmail.com>
2019-11-12 11:58:21 -0500

    implement simon round

---
+++ src/main/scala/simon_round.scala
@@ -0,0 +1,124 @@
+package SimonAcc
+
+import chisel3._
+import chisel3.util._
+
+class SimonRound(maxWordWidth: Int) extends Module {
+  val io = IO(
+    new Bundle {
+      val mode = Input(Bool())
+      val encDec = Input(Bool())
+      val block1In = Input(UInt(maxWordWidth.W))
+      val block2In = Input(UInt(maxWordWidth.W))
+      val roundKey = Input(UInt(maxWordWidth.W))
+      val iValid = Input(Bool())
+      val block1Out = Output(UInt(maxWordWidth.W))
+      val block2Out = Output(UInt(maxWordWidth.W))
+      val oValid = Output(Bool())
+      val iReady = Output(Bool())
+    })
+
+  val SIMON_64_128_WORD_SIZE = 32
+  val SIMON_128_128_WORD_SIZE = 64
+
+  val rl1_64in = RegInit(0.U(SIMON_64_128_WORD_SIZE.W))
+  val rl1_64out = Wire(UInt(SIMON_64_128_WORD_SIZE.W))
+  val rl2_64in = RegInit(0.U(SIMON_64_128_WORD_SIZE.W))
+  val rl2_64out = Wire(UInt(SIMON_64_128_WORD_SIZE.W))
+  val rl8_64in = RegInit(0.U(SIMON_64_128_WORD_SIZE.W))
+  val rl8_64out = Wire(UInt(SIMON_64_128_WORD_SIZE.W))
+
+  val rl1_128in = RegInit(0.U(SIMON_128_128_WORD_SIZE.W))
+  val rl1_128out = Wire(UInt(SIMON_128_128_WORD_SIZE.W))
+  val rl2_128in = RegInit(0.U(SIMON_128_128_WORD_SIZE.W))
+  val rl2_128out = Wire(UInt(SIMON_128_128_WORD_SIZE.W))
+  val rl8_128in = RegInit(0.U(SIMON_128_128_WORD_SIZE.W))
+  val rl8_128out = Wire(UInt(SIMON_128_128_WORD_SIZE.W))
+
+  val rl_1_64 = Module(new RotateUnit(SIMON_64_128_WORD_SIZE, 1, true))
+  val rl_2_64 = Module(new RotateUnit(SIMON_64_128_WORD_SIZE, 2, true))
+  val rl_8_64 = Module(new RotateUnit(SIMON_64_128_WORD_SIZE, 8, true))
+
+  rl_1_64.io.i_value := rl1_64in
+  rl1_64out := rl_1_64.io.o_value
+
+  rl_2_64.io.i_value := rl2_64in
+  rl2_64out := rl_2_64.io.o_value
+
+  rl_8_64.io.i_value := rl8_64in
+  rl8_64out := rl_8_64.io.o_value
+
+  val rl_1_128 = Module(new RotateUnit(SIMON_128_128_WORD_SIZE, 1, true))
+  val rl_2_128 = Module(new RotateUnit(SIMON_128_128_WORD_SIZE, 2, true))
+  val rl_8_128 = Module(new RotateUnit(SIMON_128_128_WORD_SIZE, 8, true))
+
+  rl_1_128.io.i_value := rl1_128in
+  rl1_128out := rl_1_128.io.o_value
+
+  rl_2_128.io.i_value := rl2_128in
+  rl2_128out := rl_2_128.io.o_value
+
+  rl_8_128.io.i_value := rl8_128in
+  rl8_128out := rl_8_128.io.o_value
+
+  val out = Wire(UInt(maxWordWidth.W))
+  val xIn = Wire(UInt(maxWordWidth.W))
+  val yIn = Wire(UInt(maxWordWidth.W))
+  val tmp = RegInit(0.U(maxWordWidth.W))
+  val busy = RegInit(false.B)
+  val encDecMode = RegInit(true.B)
+  val simonMode = RegInit(false.B)
+
+  when (io.encDec) {
+    xIn := io.block1In
+    yIn := io.block2In
+  }.otherwise {
+    xIn := io.block2In
+    yIn := io.block1In
+  }
+
+  when (simonMode) {
+    out := Cat(0.U(32.W), (rl1_64out & rl8_64out) ^ rl2_64out) ^ tmp
+  }.otherwise {
+    (rl1_128out & rl8_128out) ^ rl2_128out ^ tmp
+  }
+
+  // actual round process
+  when (io.iValid && !busy) {
+    io.iReady := false.B
+    busy := true.B
+    encDecMode := io.encDec
+    simonMode := io.mode
+    tmp := io.roundKey ^ yIn
+
+    when(!io.mode) {
+      rl1_64in := xIn(31, 0)
+      rl2_64in := xIn(31, 0)
+      rl8_64in := xIn(31, 0)
+    }.otherwise {
+      rl1_128in := xIn
+      rl2_128in := xIn
+      rl8_128in := xIn
+    }
+
+    when (io.encDec) {
+      io.block2Out := io.block1In
+    }.otherwise {
+      io.block1Out := io.block2In
+    }
+  }.otherwise {
+    when (busy) {
+      io.oValid := true.B
+      io.iReady := true.B
+      busy := false.B
+      when (encDecMode) {
+        io.block1Out := out
+      }.otherwise {
+        io.block2Out := out
+      }
+    }.otherwise {
+      io.oValid := false.B
+    }
+  }
+
+}
