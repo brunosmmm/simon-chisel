943c9c3725acba317af14cfa4ee963c1be06bc90
Bruno Morais <brunosmmm@gmail.com>
2019-11-15 16:02:14 -0500

    fix typo

--- src/main/scala/simon_mmio.scala
+++ src/main/scala/simon_mmio.scala
@@ -110,7 +110,7 @@ trait SimonModule extends HasRegMap {
     0x00 -> Seq(RegField(64, readSConf(_), writeSConf(_,_))),
     0x08 -> Seq(RegField(64, readKey(_), writeKeyL(_,_))),
     0x10 -> Seq(RegField(64, readKey(_), writeKeyH(_,_))),
-    0x18 -> Seq(RegField(64, readData1(_), writeData2(_,_))),
+    0x18 -> Seq(RegField(64, readData1(_), writeData1(_,_))),
     0x20 -> Seq(RegField(64, readData2(_), writeData2(_,_)))
   )
 }
