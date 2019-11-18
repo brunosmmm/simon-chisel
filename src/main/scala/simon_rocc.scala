2381dc05d7e55572aa24e211a46b1033e0f21eec
Bruno Morais <brunosmmm@gmail.com>
2019-11-18 11:18:19 -0500

    initial rocc implementation

--- src/main/scala/simon_rocc.scala
+++ src/main/scala/simon_rocc.scala
@@ -1,6 +1,7 @@
 package SimonAcc
 
 import chisel3._
+import chisel3.util._
 import freechips.rocketchip.tile._
 import freechips.rocketchip.config._
 import freechips.rocketchip.diplomacy._
@@ -26,4 +27,116 @@ class SimonRoCCModule(outer: SimonRoCC)
   // rs1 - the value of source register 1
   // rs2 - the value of source register 2
   val core = Module(new SimonCore(64, 128))
+
+  // custom functions
+  private val FUNC_INIT = 0
+  private val FUNC_ENC_ROUND = 1
+  private val FUNC_DEC_ROUND = 2
+  private val FUNC_GET_FLAGS = 3
+  private val FUNC_GET_HWORD = 4
+
+  private val SIMON_FUNCT_MODE_OFFSET = 0
+  private val SIMON_FUNCT_OP_OFFSET = 2
+  private val SIMON_FUNCT_MODE_MASK = 0x3
+  private val SIMON_FUNCT_OP_MASK = 0xC
+  private val SIMON_64_128_ROUNDS = 44
+  private val SIMON_128_128_ROUNDS = 68
+
+  val operation = (cmd.bits.inst.funct & SIMON_FUNCT_OP_MASK) >>> SIMON_FUNCT_OP_OFFSET
+  val mode = cmd.bits.inst.funct & SIMON_FUNCT_MODE_MASK
+  val kBusy = RegInit(false.B)
+  val rBusy = RegInit(false.B)
+  val hWord = RegInit(0.U(64.W))
+  val respData = RegInit(0.U(64.W))
+
+  val coreMode = RegInit(false.B)
+  val coreKeyH = RegInit(0.U(64.W))
+  val coreKeyL = RegInit(0.U(64.W))
+  val coreKeyValid = RegInit(false.B)
+  val coreEncDec = RegInit(true.B)
+  val coreDataValid = RegInit(false.B)
+  val coreData1 = RegInit(0.U(64.W))
+  val coreData2 = RegInit(0.U(64.W))
+  val coreSingle = RegInit(true.B)
+  core.io.sMode := coreMode
+  core.io.keyH := coreKeyH
+  core.io.keyL := coreKeyL
+  core.io.dEncDec := coreEncDec
+  core.io.data1In := coreData1
+  core.io.data2In := coreData2
+  core.io.dInValid := coreDataValid
+  core.io.rSingle := coreSingle
+
+  val stallResponse = Wire(Bool())
+  stallResponse = cmd.bits.inst.xd && !io.resp.ready
+
+  // auto clear
+  when (coreKeyValid) {
+    coreKeyValid := false.B
+  }
+
+  when (coreDataValid) {
+    coreDataValid := false.B
+  }
+
+  // other
+  io.interrupt := false.B
+  io.resp.bits.rd := cmd.bits.inst.rd
+  io.resp.bits.data := Mux(operation===FUNC_GET_HWORD, hWord, respData)
+  io.resp.valid := cmd.valid && cmd.bits.inst.xd && !rBusy && !stallResponse
+  io.busy := kBusy || rBusy
+  cmd.ready := !kBusy && !rBusy && !stallResponse
+
+  // receive instructions
+  when (cmd.fire()) {
+    switch (operation) {
+      is (FUNC_INIT.U) {
+        // initialize
+        coreMode := mode
+        coreKeyH := cmd.bits.rs2
+        coreKeyL := cmd.bits.rs1
+        coreKeyValid := true.B
+        kBusy := true.B
+        when (coreMode) {
+          respData := SIMON_128_128_ROUNDS
+        }.otherwise {
+          respData := SIMON_64_128_ROUNDS
+        }
+      }
+      is (FUNC_ENC_ROUND.U) {
+        coreEncDec := true.B
+        coreData1 := cmd.bits.rs1
+        coreData2 := cmd.bits.rs2
+        coreDataValid := true.B
+        rBusy := true.B
+        coreSingle := true.B
+      }
+      is (FUNC_DEC_ROUND.U) {
+        coreEncDec := false.B
+        coreData1 := cmd.bits.rs1
+        coreData2 := cmd.bits.rs2
+        coreDataValid := true.B
+        rBusy := true.B
+        coreSingle := true.B
+      }
+    }
+  }
+
+  when (kBusy) {
+    when (core.io.kExpDone) {
+      kBusy := false.B
+    }
+  }
+
+  when (rBusy) {
+    when (core.io.dOutValid) {
+      rBusy := false.B
+      when (coreMode) {
+        hWord := core.io.data2Out
+        respData := core.io.data1Out
+      }.otherwise {
+        respData := Cat(core.io.data2Out(31, 0), core.io.data1Out(31, 0))
+      }
+    }
+  }
 }
