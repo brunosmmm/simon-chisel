1d12d642cc6864454a6d3ea96c6b1efe06f05e87
Bruno Morais <brunosmmm@gmail.com>
2019-11-19 12:53:56 -0500

    logic more or less implemented, needs memory controller

--- src/main/scala/simon_toosly.scala
+++ src/main/scala/simon_toosly.scala
@@ -22,6 +22,16 @@ class SimonTooslyMemModule(implicit p: Parameters) extends LazyModule {
 
 class SimonTooslyMemModuleImp(outer: SimonTooslyMemModule)(implicit p: Parameters) extends LazyModuleImp(outer)
     with HasCoreParameters {
+  val io = IO(new Bundle
+    {
+      val wr = Input(Bool())
+      val rd = Input(Bool())
+      val rdValid = Output(Bool())
+      val ready = Ouput(Bool())
+      val addr = Input(UInt(64.W))
+      val dataIn = Input(UInt(64.W))
+      val dataOut = Output(UInt(64.W))
+    })
 
   val (mem, edge) = outer.node.out(0)
 }
@@ -45,6 +55,9 @@ class SimonTooslyModule(outer: SimonToosly)
   // memory controller
   val memCtl = outer.memCtl.module
 
+  val memWr = RegInit(false.B)
+  val memRd = RegInit(false.B)
+
   // custom functions
   private val FUNC_INIT = 0
   private val FUNC_ENC = 1
@@ -61,7 +74,9 @@ class SimonTooslyModule(outer: SimonToosly)
   val mode = io.cmd.bits.inst.funct & SIMON_FUNCT_MODE_MASK.U
   val kBusy = RegInit(false.B)
   val rBusy = RegInit(false.B)
+  val mBusy = Wire(Bool())
   val respData = RegInit(0.U(64.W))
+  mBusy := !memCtl.io.ready
 
   val coreKeyH = RegInit(0.U(64.W))
   val coreKeyL = RegInit(0.U(64.W))
@@ -88,8 +103,23 @@ class SimonTooslyModule(outer: SimonToosly)
   val stallResponse = Wire(Bool())
   val wantsResponse = RegInit(false.B)
   val responseDest = RegInit(0.U(5.W))
-  val lastOperation = RegInit(0.U(2.W))
   stallResponse := io.cmd.bits.inst.xd && !io.resp.ready
+
+  // handle encryption/decryption over memory region
+  val accBusy = RegInit(false.B)
+  val storePending = RegInit(false.B)
+  val loadPending = RegInit(false.B)
+  val storeAddr = RegInit(0.U(64.W))
+  val loadAddr = RegInit(0.U(64.W))
+  val storeWord = RegInit(0.U(64.W))
+  val pendingWordCount = RegInit(0.U(64.W))
+  val startAddr = RegInit(0.U(64.W))
+  val startEncDec = RegInit(false.B)
+
+  memCtl.io.addr := Mux(memWr, storeAddr, loadAddr)
+  memCtl.io.wr := memWr
+  memCtl.io.rd := memRd
+  memCtl.io.dataIn := storeWord
 
   // auto clear
   when (coreKeyValid) {
@@ -116,14 +146,13 @@ class SimonTooslyModule(outer: SimonToosly)
   io.resp.bits.rd := responseDest
   io.resp.bits.data := respData
   io.resp.valid := responseValid
-  io.busy := kBusy || rBusy
-  io.cmd.ready := !kBusy && !rBusy && !stallResponse
+  io.busy := kBusy || rBusy || mBusy || startEncDec
+  io.cmd.ready := !kBusy && !rBusy && !startEncDec && !mBusy && !stallResponse
 
   // receive instructions
   when (io.cmd.fire()) {
     wantsResponse := io.cmd.bits.inst.xd
     responseDest := io.cmd.bits.inst.rd
-    lastOperation := operation
     switch (operation) {
       is (FUNC_INIT.U) {
         // initialize
@@ -135,18 +164,26 @@ class SimonTooslyModule(outer: SimonToosly)
       }
       is (FUNC_ENC.U) {
         coreEncDec := true.B
-        coreData1 := io.cmd.bits.rs1(31,0)
-        coreData2 := io.cmd.bits.rs1(63, 32)
-        coreDataValid := true.B
-        rBusy := true.B
+        startAddr := io.cmd.bits.rs1
+        pendingWordCount := io.cmd.bits.rs2
+        startEncDec := true.B
+        loadPending := true.B
+        loadAddr := io.cmd.bits.rs1
       }
       is (FUNC_DEC.U) {
         coreEncDec := false.B
-        coreData1 := io.cmd.bits.rs1(31,0)
-        coreData2 := io.cmd.bits.rs1(63, 32)
-        coreDataValid := true.B
-        rBusy := true.B
+        startAddr := io.cmd.bits.rs1
+        pendingWordCount := io.cmd.bits.rs2
+        startEncDec := true.B
+        loadPending := true.B
+        loadAddr := io.cmd.bits.rs1
       }
+    }
+  }
+
+  when (startEncDec) {
+    when (pendingWordCount === 0 && !loadPending && !storePending && !rBusy) {
+      startEncDec := false.B
     }
   }
 
@@ -175,8 +212,42 @@ class SimonTooslyModule(outer: SimonToosly)
           responsePending := true.B
         }
       }
-      // respData := Cat(core.io.data2Out(31, 0), core.io.data1Out(31, 0))
+      // store to memory pending
+      storePending := true.B
+      // store to same address
+      storeAddr := loadAddr
+      storeWord := Cat(core.io.data2Out(31, 0), core.io.data1Out(31, 0))
       respData := 0.U
+    }
+  }
+
+  when (storePending) {
+    when (memCtl.io.ready) {
+      storePending := false.B
+      memWr := true.B
+
+      // prepare next load
+      when (pendingWordCount > 0) {
+        loadPending := true.B
+        loadAddr := loadAddr + 1.U
+        pendingWordCount := pendingWordCount - 1.U
+      }
+    }
+  }
+
+  when (loadPending) {
+    when (memCtl.io.ready) {
+      memRd := true.B
+      when (memRd && memCtl.io.rdValid) {
+        // read done
+        memRd := false.B
+        coreData1 := memCtl.io.dataOut(31, 0)
+        coreData2 := memCtl.io.dataOut(63, 32)
+        // perform next rounds
+        rBusy := true.B
+        coreDataValid := true.B
+        loadPending := false.B
+      }
     }
   }
 }
